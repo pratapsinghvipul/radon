8d814fd4049d772888df5427f9ac6260d2b914c9
Vipul <pratapsinghvipul@gmail.com>
2022-06-05 10:30:52 +0530

    assignment/route

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,9 +2,59 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/test-me', function (req, res) {
-    res.send('My first ever api!')
-});
+let players =
+   [
+       {
+           "name": "manish",
+           "dob": "1/1/1995",
+           "gender": "male",
+           "city": "jalandhar",
+           "sports": [
+               "swimming"
+           ]
+       },
+       {
+           "name": "gopal",
+           "dob": "1/09/1995",
+           "gender": "male",
+           "city": "delhi",
+           "sports": [
+               "soccer"
+           ]
+       },
+       
+
+{
+        "name": "lokesh",
+        "dob": "1/1/1990",
+        "gender": "male",
+        "city": "mumbai",
+        "sports": [
+            "soccer"
+        ]
+    },
+]
+
+router.post('/players', function (req, res) {
+    let newPlayer= req.body
+    let newPlayername = newPlayer.name
+    let playerStatus;
+
+    for(let i=0;i<players.length;i++){
+        if(players[i].name == newPlayername){
+            playerStatus = true
+            break;
+        }
+    }
+
+    if(playerStatus){
+        res.send("player already present")
+    }
+    else{
+        players.push(newPlayer)
+        res.send(players)
+    }
+})
 
 module.exports = router;
 // adding this comment for no reason
